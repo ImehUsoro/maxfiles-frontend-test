ed88ab78fd4ba8a7e7763ef8160cf4911672da37
Imeh Usoro <imeusoro@rocketmail.com>
2023-02-21 02:02:05 +0100

    padding and height on signup page

--- src/pages/SignUp/SignUpStyles.js
+++ src/pages/SignUp/SignUpStyles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const SignUpStyles = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
 
   a {
@@ -273,7 +273,8 @@ export const SignUpStyles = styled.div`
 
     .right-section {
       width: 100%;
-      padding: 0;
+      height: 100vh;
+      padding: 3rem 0;
       display: flex;
       flex-direction: column;
       align-items: center;
